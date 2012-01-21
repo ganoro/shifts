376daf95def74b557b2b1c7795bf075922ea2a88
royganor <roy@zend.com>
2012-01-21 22:59:07 +0200

    Change-Id: I86b772b3715a1a5f484fa467c8944dcd0c8117f1 Signed-off-by: royganor <roy@zend.com>

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -1,5 +1,10 @@
 <?php
 
+class Shifts extends Zend_Db_Table_Abstract
+{
+    protected $_name = 'shifts';
+}
+
 class IndexController extends Zend_Controller_Action
 {
 
@@ -10,9 +15,44 @@ class IndexController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        // action body
+    	$options = array(
+			'host'     => getDbHost(),
+			'username' => getDbUser(),
+			'password' => getDbPassword(),
+			'dbname'   => getDbName()
+		);
+    	$db = Zend_Db::factory('PDO_MYSQL', $options);
+		$table = new Shifts(array('db' => $db));
+		$select = $table->select();
+		var_dump($select);
+		die(1);		
     }
-
-
 }
 
+/**
+ * @return string the container db host
+ */
+function getDbHost() {
+	return get_cfg_var('zend_developer_cloud.db.host');
+}
+
+/**
+ * @return string the container db name
+ */
+function getDbName() {
+	return get_cfg_var('zend_developer_cloud.db.name');
+}
+
+/**
+ * @return string the container db user
+ */
+function getDbUser() {
+	return get_cfg_var('zend_developer_cloud.db.username');
+}
+
+/**
+ * @return string the container db password
+ */
+function getDbPassword() {
+	return get_cfg_var('zend_developer_cloud.db.password');
+}
