13374cb27413e57b9fff8da88d1829c6d6763929
royganor <roy@zend.com>
2012-01-22 00:30:40 +0200

    Change-Id: I1d8d5be0560910055de77078dd7a1aa1813a18b2 Signed-off-by: royganor <roy@zend.com>

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -1,69 +1,77 @@
 <?php
 
-class Shifts extends Zend_Db_Table_Abstract
-{
-    protected $_name = 'shifts';
+class Shifts extends Zend_Db_Table_Abstract {
+	protected $_name = 'shifts';
 }
 
-class IndexController extends Zend_Controller_Action
-{
-
-    public function init()
-    {
-        /* Initialize action controller here */
-    }
-
-    public function indexAction()
-    {
-			
-    }
-    
-    public function listAction()
-    {
-    	$month = $this->getRequest()->getParam("month");
-    	if (!$month) {
-    		$month = date("m");
-    	}
-    	
-    	$options = array(
-			'host'     => getDbHost(),
-			'username' => getDbUser(),
-			'password' => getDbPassword(),
-			'dbname'   => getDbName()
-		);
-    	$db = Zend_Db::factory('PDO_MYSQL', $options); 
-		$select = $db->select()->from("shifts")->where("MONTH(date) = $month");
-		$stmt = $db->query($select);
-		$result = $stmt->fetchAll();
-		echo json_encode($result);
-		die(0);
-    }
+class IndexController extends Zend_Controller_Action {
+	
+	public function init() {
+		$options = array ('host' => getDbHost (), 'username' => getDbUser (), 'password' => getDbPassword (), 'dbname' => getDbName () );
+		$db = Zend_Db::factory ( 'PDO_MYSQL', $options );
+	}
+	
+	public function indexAction() {
+	
+	}
+	
+	public function reportshiftAction() {
+	
+	}
+	
+	public function reportsessionAction() {
+	
+	}
+	
+	public function removeshiftAction() {
+	
+	}
+	
+	public function removesessionAction() {
+	
+	}
+	
+	public function listAction() {
+		$month = $this->getRequest ()->getParam ( "month" );
+		if (! $month) {
+			$month = date ( "m" );
+		}
+		$select = $this->db->select ()->from ( "shifts" )->where ( "MONTH(date) = $month" );
+		$stmt = $this->db->query ( $select );
+		$result = $stmt->fetchAll ();
+		echo json_encode ( $result );
+		die ( 0 );
+	}
 }
 
 /**
+ *
  * @return string the container db host
  */
 function getDbHost() {
-	return get_cfg_var('zend_developer_cloud.db.host');
+	return get_cfg_var ( 'zend_developer_cloud.db.host' );
 }
 
 /**
+ *
  * @return string the container db name
  */
 function getDbName() {
-	return get_cfg_var('zend_developer_cloud.db.name');
+	return get_cfg_var ( 'zend_developer_cloud.db.name' );
 }
 
 /**
+ *
  * @return string the container db user
  */
 function getDbUser() {
-	return get_cfg_var('zend_developer_cloud.db.username');
+	return get_cfg_var ( 'zend_developer_cloud.db.username' );
 }
 
 /**
+ *
  * @return string the container db password
  */
 function getDbPassword() {
-	return get_cfg_var('zend_developer_cloud.db.password');
+	return get_cfg_var ( 'zend_developer_cloud.db.password' );
 }
