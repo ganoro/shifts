26df1a57507b161ce31f1dd1c89146098b5075e2
royganor <roy@zend.com>
2012-01-26 10:45:05 +0200

    Change-Id: Idc2df60c951fbe8a6e6d907abf8993eda5d8a12d Signed-off-by: royganor <roy@zend.com>

---
+++ public/scripts/index/index.js
@@ -0,0 +1,19 @@
+$(document).ready(
+		function() {
+			$.getJSON('index/list', function(data) {
+				var items = [];
+
+				$.each(data, function(key, val) {
+					d = new Date(Date.parse(val.date));
+					if (val.type != "sessions") {
+						items.push('<li data-theme="b"><a href="index.html" >'
+								+ d.toLocaleDateString() + '</a></li>');
+					} else {
+						items.push('<li data-theme="c"><a href="index.html" >'
+								+ d.toLocaleDateString() + '</a></li>');
+					}
+				});
+				$("#mainList").append(items.join(''));
+				$('#mainList').listview('refresh');
+			});
+		});
