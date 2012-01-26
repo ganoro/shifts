ca47e4826f2d7b7cc4ff7a5349ff4314056ff644
royganor <roy@zend.com>
2012-01-26 11:02:09 +0200

    Change-Id: I33da48973ca4b8768d17cf0abd775bc588361f83 Signed-off-by: royganor <roy@zend.com>

---
+++ public/scripts/index/insert.js
@@ -0,0 +1,76 @@
+function getSelected(options) {
+	for ( var i = 0; i < options.length; i++) {
+		var e = options[i];
+		if (e.selected) {
+			return e.value;
+		}
+	}
+}
+
+$(document)
+		.ready(
+				function() {
+					$(document).bind("mobileinit", function() {
+						$.mobile.ajaxEnabled = false;
+					});
+
+					var d = new Date();
+					$('#select-choice-month').children()[d.getMonth() + 1].selected = true;
+					$('#select-choice-day').children()[d.getDate()].selected = true;
+					$('#select-choice-year').children()[d.getFullYear() - 2011].selected = true;
+					$('#select-choice-month').selectmenu('refresh');
+					$('#select-choice-day').selectmenu('refresh');
+					$('#select-choice-year').selectmenu('refresh');
+
+					$("#save-btn").bind(
+							"click",
+							function(event, ui) {
+								y = getSelected($('#select-choice-year')
+										.children());
+								m = getSelected($('#select-choice-month')
+										.children());
+								d = getSelected($('#select-choice-day')
+										.children());
+								c = $('#comments').value;
+
+								if ($("#radio-choice-1").checked == "checked") {
+									// Shift
+									$.ajax({
+										type : 'POST',
+										url: 'index/reportshift',
+										data : {
+											'm' : m,
+											'd' : d,
+											'y' : y,
+											'c' : c
+										},
+										success : function(m) {
+											alert(m);
+										},
+										error : function(jqXHR, textStatus,
+												errorThrown) {
+											alert(textStatus);
+										}
+
+									});
+								} else {
+									// Sessions
+									$.ajax({
+										type : 'POST',
+										url: 'index/reportsession',
+										data : {
+											'm' : m,
+											'd' : d,
+											'y' : y,
+											'c' : c
+										},
+										success : function(m) {
+										},
+										error : function(jqXHR, textStatus,
+												errorThrown) {
+										}
+									});
+								}
+								return true;
+							});
+				});
