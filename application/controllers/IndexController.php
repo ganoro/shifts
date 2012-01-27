25ed57590bf95ebe0249b53712ab2773033704f1
royganor <roy@zend.com>
2012-01-28 01:15:12 +0200

    Change-Id: I834aed80367ec59d62e0f67a81277fa86f4138ad Signed-off-by: royganor <roy@zend.com>

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -21,7 +21,7 @@ class IndexController extends Zend_Controller_Action {
 		
 		// month setting
 		$this->month = $this->getRequest()->getParam ( "m" );
-		$this->month = $this->getRequest()->getParam ( "y" );
+		$this->year = $this->getRequest()->getParam ( "y" );
 		if (!$this->month) {
 			$this->month = date ( "m" );
 			$this->year = date ( "Y" );
