54c18044cc8f897dea3318e7662d900e95650c2c
royganor <roy@zend.com>
2012-01-28 00:42:07 +0200

    Change-Id: I8a7d02aaaba103bfbcd5afcc0e613263fde21595 Signed-off-by: royganor <roy@zend.com>

--- public/scripts/index/index.js
+++ public/scripts/index/index.js
@@ -1,20 +1,51 @@
-$(document).ready(
-		function() {
-			$.getJSON('index/list', function(data) {
-				var items = [];
+$(document)
+		.ready(
+				function() {
+					$
+							.getJSON(
+									require.toUrl('index/list/.').slice(0, -2),
+									{
+										tags : "cat",
+										tagmode : "any",
+										format : "json"
+									},
 
-				$.each(data, function(key, val) {
-					d = new moment([val.date.substr(0,4), val.date.substr(5,2), val.date.substr(8,2)]);
-					
-					if (val.type != "session") {
-						items.push('<li data-theme="b"><a href="index.html" >'
-								+ d.format('dddd, MMMM Do YYYY') + '</a></li>');
-					} else {
-						items.push('<li data-theme="c"><a href="index.html" >'
-								+ d.format('dddd, MMMM Do YYYY') + '</a></li>');
-					}
+									function(data) {
+										var items = [];
+										$
+												.each(
+														data,
+														function(key, val) {
+															d = new moment(
+																	[
+																			val.date
+																					.substr(
+																							0,
+																							4),
+																			val.date
+																					.substr(
+																							5,
+																							2),
+																			val.date
+																					.substr(
+																							8,
+																							2) ]);
+
+															if (val.type != "session") {
+																items
+																		.push('<li data-theme="b"><a href="index.html" >'
+																				+ d
+																						.format('dddd, MMMM Do YYYY')
+																				+ '</a></li>');
+															} else {
+																items
+																		.push('<li data-theme="c"><a href="index.html" >'
+																				+ d
+																						.format('dddd, MMMM Do YYYY')
+																				+ '</a></li>');
+															}
+														});
+										$("#mainList").append(items.join(''));
+										$('#mainList').listview('refresh');
+									});
 				});
-				$("#mainList").append(items.join(''));
-				$('#mainList').listview('refresh');
-			});
-		});
