6bb4c41b992daf7aad862c1a5fa734433f596390
royganor <roy@zend.com>
2012-01-28 18:38:01 +0200

    Change-Id: Ia47f18192fceb4c604dcb8deedf4ed910811a3a0 Signed-off-by: royganor <roy@zend.com>

--- public/scripts/index/index.js
+++ public/scripts/index/index.js
@@ -1,51 +1,25 @@
-$(document)
-		.ready(
-				function() {
-					$
-							.getJSON(
-									require.toUrl('index/list/.').slice(0, -1)
-											+ "m/" + $("#month").html() + "/y/"
-											+ $("#year").html(),
-									function(data) {
-										var items = [];
-										$
-												.each(
-														data,
-														function(key, val) {
-															d = new moment(
-																	[
-																			val.date
-																					.substr(
-																							0,
-																							4),
-																			-1
-																					+ 1
-																					* val.date
-																							.substr(
-																									5,
-																									2),
-																			-1
-																					+ 1
-																					* val.date
-																							.substr(
-																									8,
-																									2) ]);
+$(document).ready(
+		function() {
+			$.getJSON(require.toUrl('index/list/.').slice(0, -1) + "m/"
+					+ $("#month").html() + "/y/" + $("#year").html(), function(
+					data) {
+				var items = [];
+				$.each(data, function(key, val) {
+					var d = moment(val.date, [ "YYYY-MM-DD" ]);
+					var updateURL = require.toUrl('index/update/.')
+							.slice(0, -1)
+							+ "d/"
+							+ d.format("DD")
+							+ "/m/"
+							+ d.format("MM")
+							+ "/y/" + d.format("YYYY");
 
-															if (val.type != "session") {
-																items
-																		.push('<li data-theme="b"><a href="index.html" >'
-																				+ d
-																						.format('dddd, MMMM Do YYYY')
-																				+ '</a></li>');
-															} else {
-																items
-																		.push('<li data-theme="c"><a href="index.html" >'
-																				+ d
-																						.format('dddd, MMMM Do YYYY')
-																				+ '</a></li>');
-															}
-														});
-										$("#mainList").append(items.join(''));
-										$('#mainList').listview('refresh');
-									});
+					items.push('<li data-theme="'
+							+ (val.type != "session" ? 'b' : 'c')
+							+ '"><a href="' + updateURL + '" >'
+							+ d.format('dddd, MMMM Do YYYY') + '</a></li>');
 				});
+				$("#mainList").append(items.join(''));
+				$('#mainList').listview('refresh');
+			});
+		});
